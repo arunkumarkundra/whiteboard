52eb184d88db1eaa3a82f41401399f4f571d531e
arunkumarkundra <arunkumarkundra@gmail.com>
2025-03-23 17:20:15 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Whiteboard from './Whiteboard';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Whiteboard />
+    </div>
+  );
+}
+
+export default App;
